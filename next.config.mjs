30df4f9c669615de393b6f641804dbfaf6b34cde
Firdavs Karimov <mr.firdavs2003.bek@gmail.com>
2025-11-13 16:52:03 +0500

    added algebras next js auto translate

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import autoIntl, { LanguageCode } from "algebras-auto-intl";
+
+const nextConfig = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
+
+// autoIntl bilan configni o'rash
+export default autoIntl({
+  defaultLocale: LanguageCode.en,
+  includeNodeModules: false,
+  targetLocales: [LanguageCode.es, LanguageCode.fr, LanguageCode.de],
+  outputDir: "src/intl",
+})(nextConfig);

eed426a3005694355cf9f60fb36ccc0f1b3cf486
Jakhongirbek <jakhonyokubjonov@gmail.com>
2026-01-12 17:47:29 +0900

    fix:Remove i18next cli and Integrating with Algebras-Intl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,39 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import AlgebrasIntlProvider from "algebras-auto-intl/runtime/server";
+import IntlWrapper from '@dima-algebras/algebras-auto-intl/runtime/server'
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
 
-import "./globals.css";
+import './globals.css'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "FAERTAG IN | Партнёрство и реклама на YouTube",
-  description: "Продвиньте свой бренд среди активной аудитории Армении и России. Партнёрские видео, интеграции и имиджевые кампании на YouTube-канале FAERTAG IN",
-};
+	title: 'FAERTAG IN | Партнёрство и реклама на YouTube',
+	description:
+		'Продвиньте свой бренд среди активной аудитории Армении и России. Партнёрские видео, интеграции и имиджевые кампании на YouTube-канале FAERTAG IN',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="ru">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-         <AlgebrasIntlProvider>
-          {children}
-          
-        </AlgebrasIntlProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='ru'>
+			<IntlWrapper>
+				<body
+					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				>
+					{children}
+				</body>
+			</IntlWrapper>
+		</html>
+	)
 }
